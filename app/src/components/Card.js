0c4b7d801b452e893701868941ea568a3845cc16
kip2 <tqa1no4@gmail.com>
2023-10-26 23:34:23 +0900

    カードレイアウトの調整

--- app/src/components/Card.js
+++ app/src/components/Card.js
@@ -3,8 +3,8 @@ import styles from "../css/styles.css";
 
 export default function Card({ name, calorie, macro, path }) {
     return (
-        <section>
-            <img className={styles.card} src={path} />
+        <section className={styles.card}>
+            <img src={path} />
             <h1>{name}</h1>
             <p>Calorie: {calorie}</p>
             <p>Macro: {macro}</p>
--- app/src/components/FruitsList.js
+++ app/src/components/FruitsList.js
@@ -6,7 +6,7 @@ export default function FruitList({ fruits }) {
     return(
         <div className={sytles.background}>
             <h1 className={sytles.title}>Fruit List</h1>
-            <div>
+            <div className={sytles.container}>
                 {
                     fruits.map(fruit => <Card {...fruit} />)
                 }
