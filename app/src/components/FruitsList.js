979d94ca8c21296406bb927fc9fa9772ed9bd1d3
kip2 <tqa1no4@gmail.com>
2023-10-26 23:58:32 +0900

    fix: レイアウト調整

--- app/src/components/FruitsList.js
+++ app/src/components/FruitsList.js
@@ -5,7 +5,9 @@ import sytles from "../css/styles.css";
 export default function FruitList({ fruits }) {
     return(
         <div className={sytles.background}>
-            <h1 className={sytles.title}>Fruit List</h1>
+            <div className={sytles.title_wrapper}>
+                <h1 className={sytles.title}>Fruit List</h1>
+            </div>
             <div className={sytles.container}>
                 {
                     fruits.map(fruit => <Card {...fruit} />)
