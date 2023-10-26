57548e0b2839594befdc866fb9b769447d40e681
kip2 <tqa1no4@gmail.com>
2023-10-26 22:09:58 +0900

    add: fruitslist.jsonの呼び出し

--- Fruit-List/src/index.js
+++ Fruit-List/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import FruitList from "./components/FruitsList";
+import fruitList from "./json/fruits.json";
 
 
 render(<FruitList fruitList={fruitList}/>, document.getElementById("root"));
