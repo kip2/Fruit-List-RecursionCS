f316eaf6807a0d5b49c05cecfefa3b5fc414970b
kip2 <tqa1no4@gmail.com>
2023-10-26 22:58:17 +0900

    大枠のレイアウト作成

--- app/src/components/FruitsList.js
+++ app/src/components/FruitsList.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Card from "./Card";
+import sytles from "../css/styles.css";
 
 export default function FruitList({ fruits }) {
     return(
-        <>
-            <h1>Fruit List</h1>
+        <div className={sytles.background}>
+            <h1 className={sytles.title}>Fruit List</h1>
             <div>
                 {
                     fruits.map(fruit => <Card {...fruit} />)
                 }
             </div>
-        </>
+        </div>
     );
 }
